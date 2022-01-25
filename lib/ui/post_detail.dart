e16533fc00f3279cbba482e05c79341cf7efdf38
Yubaraj Poudel <yubarajpoudel708@gmail.com>
2022-01-25 15:43:42 +0400

    fixed

--- lib/ui/post_detail.dart
+++ lib/ui/post_detail.dart
@@ -8,14 +8,49 @@ class PostDetail extends StatelessWidget {
   static const String TAG = "postDetail";
   Post? post;
   Comment? commentNotifier;
+  var commentController = TextEditingController();
 
   PostDetail({Key? key, this.post}) : super(key: key);
 
   _commentItem(Comment comment) {
-    return ListTile(
-      title: Text(comment.email!),
-      subtitle: Text(comment.body!),
+    return Card(
+      color: Colors.blueGrey,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+      child: ListTile(
+        title: Text(
+          comment.email!,
+          style: const TextStyle(color: Colors.white),
+        ),
+        subtitle: Text(
+          comment.body!,
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
     );
+  }
+
+  _addComment() {
+    return Card(
+        color: Colors.white,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+        child: Row(
+          children: [
+            Expanded(
+                child: TextFormField(
+              controller: commentController,
+              validator: (value) =>
+                  value != null && value.isEmpty ? "* Required" : null,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'comments here'),
+            )),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.send,
+                ))
+          ],
+        ));
   }
 
   @override
@@ -45,36 +80,58 @@ class PostDetail extends StatelessWidget {
                 style:
                     const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-              Text(post!.body ?? "", textAlign: TextAlign.left,),
+              Text(
+                post!.body ?? "",
+                textAlign: TextAlign.left,
+              ),
               const SizedBox(
                 height: 8.0,
               ),
-              Consumer<Comment>(
-                builder: (context, comment, child) {
-                  if (comment.connectionStatus == ConnectionStatus.LOADING) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (comment.connectionStatus ==
-                      ConnectionStatus.ERROR) {
-                    return Center(
-                      child: Text(comment.errorMessage ?? "Unknown error"),
-                    );
-                  } else {
-                    return SingleChildScrollView(
-                      child: ListView.builder(
-                          itemCount: comment.commentList!.length,
-                          shrinkWrap: true,
-                          itemBuilder: (context, pos) {
-                            return _commentItem(comment.commentList![pos]);
-                          }),
-                    );
-                  }
-                },
-              )
+              Expanded(
+                flex: 1,
+                child: Stack(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height,
+                      child: Consumer<Comment>(
+                        builder: (context, comment, child) {
+                          if (comment.connectionStatus ==
+                              ConnectionStatus.LOADING) {
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          } else if (comment.connectionStatus ==
+                              ConnectionStatus.ERROR) {
+                            return Center(
+                              child:
+                                  Text(comment.errorMessage ?? "Unknown error"),
+                            );
+                          } else {
+                            return ListView.builder(
+                                itemCount: comment.commentList!.length + 1,
+                                itemBuilder: (context, pos) {
+                                  if (pos == comment.commentList!.length) {
+                                    return const SizedBox(
+                                      height: 70,
+                                    );
+                                  } else {
+                                    return _commentItem(
+                                        comment.commentList![pos]);
+                                  }
+                                });
+                          }
+                        },
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: _addComment(),
+                    )
+                  ],
+                ),
+              ),
             ],
-          ),
-      ),
+          )),
     );
   }
 }
